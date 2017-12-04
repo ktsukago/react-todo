778c914fd55463bb42ee86575462b317bc1aea13
EC2 Default User <ec2-user@ip-172-31-87-31.ec2.internal>
2017-12-04 13:09:10 +0000

    add material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+
 
 const App = () => (
   <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
+    <MuiThemeProvider>
+      <AppBar title="TODO LIST"/>
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    </MuiThemeProvider>
   </div>
 )
 
