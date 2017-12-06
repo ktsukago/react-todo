39cd212961fdcfb1446f44f4028ee4c95967f125
code_commit_guest-at-057241238874 <tsukagk@acbc329cfcdf.ant.amazon.com>
2017-12-06 11:06:26 +0900

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
 
-
-const App = () => (
-  <div>
-    <MuiThemeProvider>
-      <AppBar title="TODO LIST"/>
-      <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-      </div>
-    </MuiThemeProvider>
-  </div>
-)
+class App extends Component {
+    render() {
+        Analytics.record('appRender');
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <AppBar title="TODO LIST"/>
+                    <div>
+                        <AddTodo/>
+                        <VisibleTodoList/>
+                        <Footer/>
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        )
+    }
+}
 
 export default App
