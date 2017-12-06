f209ada7f24a30e2582903a64950cd95260e9e05
code_commit_guest-at-057241238874 <tsukagk@acbc329cfcdf.ant.amazon.com>
2017-12-06 11:16:13 +0900

    analytics

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+import Amplify from 'aws-amplify'
+import { Analytics } from 'aws-amplify';
+import aws_exports from '../configuration/aws-exports'
 
 
-const App = () => (
-  <div>
-    <MuiThemeProvider>
-      <AppBar title="TODO LIST"/>
-      <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-      </div>
-    </MuiThemeProvider>
-  </div>
-)
+Amplify.configure(aws_exports);
+
+
+class App extends Component {
+    render() {
+        Analytics.record('appRender');
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <AppBar title="TODO LIST"/>
+                    <div>
+                        <AddTodo/>
+                        <VisibleTodoList/>
+                        <Footer/>
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        )
+    }
+}
 
 export default App
